afd8fe9e8f9da5134ac96eb0785a34b94f2da761
Selim Şahin <selimsahin.sns@gmail.com>
2024-03-09 19:38:21 +0300

    Initialize Log Aggregation Service via Spring Initializr.

---
+++ log-aggregation-service/src/main/java/com/selimsahin/logaggregationservice/LogAggregationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.selimsahin.logaggregationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogAggregationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogAggregationServiceApplication.class, args);
+	}
+
+}
