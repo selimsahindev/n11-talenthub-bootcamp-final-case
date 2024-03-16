0d45b70cbe82f6f3ddb651aafed1295843c5d96b
Selim Şahin <selimsahin.sns@gmail.com>
2024-03-17 00:06:55 +0300

    Initialized front end Nextjs project.

---
+++ nextjs-frontend/src/app/page.tsx
@@ -0,0 +1,7 @@
+import Link from "next/link";
+
+export default function HomePage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white"></main>
+  );
+}
